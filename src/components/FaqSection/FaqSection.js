e0b91c1fb6645b7451ba7b2096f5d4adcc1382c0
Divyansh Vaish <dvisro13@gmail.com>
2025-05-09 16:13:10 +0530

    done

---
+++ src/components/FaqSection/FaqSection.js
@@ -0,0 +1,162 @@
+import React from "react";
+// Link is removed as it's not used by the new FAQ content
+// import { Link } from "react-router-dom";
+// Team API import is removed
+// import Team from "../../api/team";
+
+// MUI Accordion imports
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+
+import shape1 from "../../images/team/shape-1.svg";
+import shape2 from "../../images/team/shape-2.svg";
+import shape3 from "../../images/team/shape-3.svg";
+import shape4 from "../../images/team/shape-4.svg";
+
+// ClickHandler is removed as it's not used by the new FAQ content
+// const ClickHandler = () => {
+//   window.scrollTo(10, 0);
+// };
+
+const TeamSection = (props) => {
+  // You might consider renaming this component to FaqSection for clarity
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <section className={`wpo-team-section section-padding ${props.pbClass}`}>
+      <div className="container">
+        <div className="wpo-section-title-s2">
+          <small>FAQ's</small>
+          <h2>
+            Frequently&nbsp;
+            <span>
+              Asked
+              <i className="shape">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 206 53"
+                  fill="none">
+                  <path d="M152.182 2.58319C107.878 0.889793 54.8748 6.13932 21.2281 18.6943C14.2699 21.4407 7.93951 24.7738 5.70192 28.7128C4.27488 31.2398 5.03121 33.954 7.69121 36.2925C14.8835 42.3911 31.9822 45.4011 46.8006 47.3115C78.3067 51.0179 113.672 51.7406 145.489 48.3204C167.194 46.0009 200.667 39.5923 199.399 28.5709C198.543 20.0621 180.437 14.5729 162.979 11.6178C138.219 7.469 111.131 6.00576 84.5743 5.86862C71.32 5.85789 58.0913 6.85723 45.6675 8.78436C33.512 10.7186 21.2709 13.4317 12.6602 17.5817C11.2246 18.2877 8.62449 17.4553 9.9973 16.6813C20.7486 11.2493 38.0215 7.73493 53.9558 5.76368C77.1194 2.90994 101.75 3.75426 125.339 5.14356C158.167 7.2615 207.554 13.5139 204.928 30.7413C203.629 36.0898 194.762 40.5057 184.681 43.5503C156.563 51.768 119.114 53.6844 85.6331 52.5265C65.1694 51.7477 44.4831 50.1855 25.9972 46.2442C11.4129 43.1186 -1.0337 37.8297 0.0679738 30.5063C2.14003 19.9035 31.4913 12.0006 52.6201 7.98775C71.2971 4.45904 91.3384 2.2302 111.674 1.24636C125.228 0.595237 138.962 0.539188 152.536 1.15931C153.475 1.20224 154.154 1.55523 154.051 1.94876C153.951 2.33872 153.115 2.62135 152.182 2.58319Z" />
+                </svg>
+              </i>
+            </span>
+            &nbsp;Questions
+          </h2>
+        </div>
+        {/* Replacing wpo-team-wrap with FAQ content structure */}
+        <div className="faq-accordion-wrap">
+          {" "}
+          {/* You can use a more relevant class name here */}
+          <div className="row justify-content-center">
+            {" "}
+            {/* Optional: for centering the accordion block */}
+            <div className="col-lg-10 col-12">
+              {" "}
+              {/* Adjust column width as needed */}
+              {/* Copied from FaqSection */}
+              <div className="wpo-benefits-item">
+                {" "}
+                {/* This class came from the FaqSection snippet, keep or change as per your styling */}
+                <Accordion
+                  expanded={expanded === "panel1"}
+                  onChange={handleChange("panel1")}>
+                  <AccordionSummary
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header">
+                    <Typography>
+                      Market research on our global panel with support from our
+                      experts.
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Eum exercitationem pariatur iure nemo esse repellendus est
+                      quo recusandae. Delectus, maxime.
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion
+                  expanded={expanded === "panel2"}
+                  onChange={handleChange("panel2")}>
+                  <AccordionSummary
+                    aria-controls="panel2bh-content"
+                    id="panel2bh-header">
+                    <Typography>
+                      Planning can help alleviate workplace stress and increase
+                      productivity.
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Eum exercitationem pariatur iure nemo esse repellendus est
+                      quo recusandae. Delectus, maxime.
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion
+                  expanded={expanded === "panel3"}
+                  onChange={handleChange("panel3")}>
+                  <AccordionSummary
+                    aria-controls="panel3bh-content"
+                    id="panel3bh-header">
+                    <Typography>
+                      Those who experiment the most, are able to innovate the
+                      best.
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Eum exercitationem pariatur iure nemo esse repellendus est
+                      quo recusandae. Delectus, maxime.
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion
+                  expanded={expanded === "panel4"}
+                  onChange={handleChange("panel4")}>
+                  <AccordionSummary
+                    aria-controls="panel4bh-content"
+                    id="panel4bh-header">
+                    <Typography>
+                      Understand Your Problem, You must understand the issue.
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Eum exercitationem pariatur iure nemo esse repellendus est
+                      quo recusandae. Delectus, maxime.
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="shape-1">
+        <img src={shape1} alt="" />
+      </div>
+      <div className="shape-2">
+        <img src={shape2} alt="" />
+      </div>
+      <div className="shape-3">
+        <img src={shape3} alt="" />
+      </div>
+      <div className="shape-4">
+        <img src={shape4} alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default TeamSection;
--- src/main-component/HomePage/HomePage.js
+++ src/main-component/HomePage/HomePage.js
@@ -1,41 +1,43 @@
-import React, {Fragment} from 'react';
-import Navbar from '../../components/Navbar/Navbar'
-import Hero from '../../components/hero/hero';
-import About from '../../components/about/about';
-import WhyCentury from '../../components/WhyCentury/WhyCentury';
-import WhatdoWeOffer from '../../components/WhatDoWeOffer/WhatDoWeOffer';
-import WhatdoWeOffer2 from '../../components/WhatDoWeOffer2/WhatDoWeOffer2';
-import OurMIssion from '../../components/OurMission/OurMission';
-import CategorySection from '../../components/CategorySection/CategorySection';
-import CourseSection from '../../components/CourseSection/CourseSection';
-import Testimonial from '../../components/Testimonial/Testimonial';
-import TeamSection from '../../components/TeamSection/TeamSection';
-import ChooseSection from '../../components/ChooseSection/ChooseSection';
-import BlogSection from '../../components/BlogSection/BlogSection';
-import Newslatter from '../../components/Newslatter/Newslatter';
-import Scrollbar from '../../components/scrollbar/scrollbar';
-import Footer from '../../components/footer/Footer';
+import React, { Fragment } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Hero from "../../components/hero/hero";
+import About from "../../components/about/about";
+import WhyCentury from "../../components/WhyCentury/WhyCentury";
+import WhatdoWeOffer from "../../components/WhatDoWeOffer/WhatDoWeOffer";
+import WhatdoWeOffer2 from "../../components/WhatDoWeOffer2/WhatDoWeOffer2";
+import OurMIssion from "../../components/OurMission/OurMission";
+import FaqSection from "../../components/FaqSection/FaqSection";
+// import CategorySection from "../../components/CategorySection/CategorySection";
+// import CourseSection from "../../components/CourseSection/CourseSection";
+// import Testimonial from "../../components/Testimonial/Testimonial";
+// import TeamSection from "../../components/TeamSection/TeamSection";
+// import ChooseSection from "../../components/ChooseSection/ChooseSection";
+// import BlogSection from "../../components/BlogSection/BlogSection";
+import Newslatter from "../../components/Newslatter/Newslatter";
+import Scrollbar from "../../components/scrollbar/scrollbar";
+import Footer from "../../components/footer/Footer";
 
-const HomePage =() => {
-    return (
-      <Fragment>
-        <Navbar hclass={"wpo-header-style-4"} />
-        <Hero />
-        <About />
-        <WhyCentury />
-        <WhatdoWeOffer />
-        <WhatdoWeOffer2 />
-        <OurMIssion />
-        {/* <CategorySection/> */}
-        <CourseSection />
-        <Testimonial />
-        <TeamSection pbClass={"pb-big"} />
-        <ChooseSection />
-        <BlogSection />
-        <Newslatter />
-        <Footer />
-        <Scrollbar />
-      </Fragment>
-    );
+const HomePage = () => {
+  return (
+    <Fragment>
+      <Navbar hclass={"wpo-header-style-4"} />
+      <Hero />
+      <About />
+      <WhyCentury />
+      <WhatdoWeOffer />
+      <WhatdoWeOffer2 />
+      <OurMIssion />
+      <FaqSection />
+      {/* <CategorySection/> */}
+      {/* <CourseSection />
+      <Testimonial />
+      <TeamSection pbClass={"pb-big"} />
+      <ChooseSection />
+      <BlogSection /> */}
+      <Newslatter />
+      <Footer />
+      <Scrollbar />
+    </Fragment>
+  );
 };
 export default HomePage;
