f8fd413d9283765c2bd4f9a46097ddfe1e8260a8
Divyansh Vaish <rksrivastava9890@gmail.com>
2025-05-22 17:47:50 +0530

    done

--- frontend/src/api/CoursesCategory.js
+++ frontend/src/api/CoursesCategory.js
@@ -12,8 +12,8 @@ const CoursesCategory = [
     Id: "1",
     cImg: cimg1,
     thumb: "flaticon-user-experience",
-    title: "Equity Market",
-    slug: "UI-UX-Design",
+    title: "Smart Saver",
+    slug: "/pdf/smart-saver.pdf",
     //  description:
     //    "We are providing you the best UI/UX design guideline. That help you be professional.",
   },
@@ -21,8 +21,8 @@ const CoursesCategory = [
     Id: "2",
     cImg: cimg2,
     thumb: "flaticon-megaphone",
-    title: "Commodity Market",
-    slug: "Digital-Marketing",
+    title: "Money Max",
+    slug: "/pdf/money-max.pdf",
     //  description:
     //    "We are providing you the best Digital Marketing guideline. That help you be professional. ",
   },
@@ -30,8 +30,8 @@ const CoursesCategory = [
     Id: "3",
     cImg: cimg3,
     thumb: "flaticon-code",
-    title: "Foreign Market",
-    slug: "Development",
+    title: "Money Kick Start",
+    slug: "/pdf/fast-track.pdf",
     //  description:
     //    "We are providing you are the best Development guideline for. That help you be professional.",
   },
@@ -39,8 +39,8 @@ const CoursesCategory = [
     Id: "4",
     cImg: cimg4,
     thumb: "flaticon-knowledge",
-    title: "Finance",
-    slug: "Self-Improvement",
+    title: "Wealth Boost",
+    slug: "/pdf/wealth-boost.pdf",
     //  description:
     //    "We are providing you the best Self Improvement guideline. That help you be professional.",
   },
@@ -48,8 +48,8 @@ const CoursesCategory = [
     Id: "5",
     cImg: cimg5,
     thumb: "flaticon-video-lesson",
-    title: "Investment",
-    slug: "Video-Editing",
+    title: "Risch Boost",
+    slug: "/pdf/rich-boast.pdf",
     //  description:
     //    "We are providing you the best Video Editing guideline for you. That help you be professional.",
   },
@@ -57,35 +57,8 @@ const CoursesCategory = [
     Id: "6",
     cImg: cimg6,
     thumb: "flaticon-paint-palette",
-    title: "Training Program",
-    slug: "Graphic-Design",
-    //  description:
-    //    "We are providing you the best Graphic Design guideline. That help you be professional.",
-  },
-  {
-    Id: "7",
-    cImg: cimg6,
-    thumb: "flaticon-paint-palette",
-    title: "Research Technical Call",
-    slug: "Graphic-Design",
-    //  description:
-    //    "We are providing you the best Graphic Design guideline. That help you be professional.",
-  },
-  {
-    Id: "8",
-    cImg: cimg6,
-    thumb: "flaticon-paint-palette",
-    title: "Intraday Paid Call",
-    slug: "Graphic-Design",
-    //  description:
-    //    "We are providing you the best Graphic Design guideline. That help you be professional.",
-  },
-  {
-    Id: "9",
-    cImg: cimg6,
-    thumb: "flaticon-paint-palette",
-    title: "Trade Advisory Plan,",
-    slug: "Graphic-Design",
+    title: "Fast Track",
+    slug: "/pdf/fast-track.pdf",
     //  description:
     //    "We are providing you the best Graphic Design guideline. That help you be professional.",
   },
--- frontend/src/main-component/Investment/Investment.js
+++ frontend/src/main-component/Investment/Investment.js
@@ -4,8 +4,19 @@ import Navbar from "../../components/Navbar/Navbar";
 import PageTitle from "../../components/pagetitle/PageTitle";
 import Scrollbar from "../../components/scrollbar/scrollbar";
 import Footer from "../../components/footer/Footer";
+import CoursesCategory from "../../api/CoursesCategory"; // Your data file
+import cImag from "../../images/shape/1.svg";
+import cImag2 from "../../images/shape/2.svg";
+import cImag3 from "../../images/shape/3.svg";
+import cImag4 from "../../images/shape/4.svg";
+// Remove 'Link' from 'react-router-dom' if no longer used elsewhere for this specific functionality
+// import { Link } from "react-router-dom";
 
-const Investment = () => {
+const ClickHandler = () => {
+  window.scrollTo(10, 0);
+};
+
+const Investment = (props) => {
   return (
     <Fragment>
       <Helmet>
@@ -23,6 +34,56 @@ const Investment = () => {
 
       <Navbar />
       <PageTitle pageTitle={"Investment"} pagesub={"Investment"} />
+      <section
+        className={`wpo-courses-section-s2 section-padding ${props.cClass}`}>
+        <div className="container">
+          <div className="row wpo-courses-wrap">
+            {CoursesCategory.map((category, aitem) => (
+              <div
+                className={`col-lg-4 col-md-6 col-12 grid s${category.Id}`}
+                key={aitem} // It's generally better to use category.Id for keys if they are unique
+              >
+                <div className="wpo-courses-item">
+                  <div className="wpo-courses-text">
+                    <div className="courses-icon">
+                      {/* You can use category.thumb here if it's meant for the class */}
+                      <i className={`fi ${category.thumb}`}></i>
+                    </div>
+                    <h2>
+                      {/* Changed Link to a tag for download */}
+                      <a
+                        onClick={ClickHandler} // This will scroll the page before download starts
+                        href={category.slug} // Path to your PDF in the public folder
+                        download // This attribute triggers the download
+                        // Optionally, suggest a filename: download={`${category.title.replace(/\s+/g, '_')}.pdf`}
+                      >
+                        {category.title}
+                      </a>
+                    </h2>
+                    {/* Ensure your CoursesCategory data has a description property */}
+                    {/* <p>{category.description}</p> */}
+                    {/* If description is commented out in your data, this will show nothing. */}
+                    {/* You can uncomment it in CoursesCategory.js if needed. */}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="shape-1">
+          <img src={cImag} alt="" />
+        </div>
+        <div className="shape-2">
+          <img src={cImag2} alt="" />
+        </div>
+        <div className="shape-3">
+          <img src={cImag3} alt="" />
+        </div>
+        <div className="shape-4">
+          <img src={cImag4} alt="" />
+        </div>
+      </section>
+
       <Footer />
       <Scrollbar />
     </Fragment>
